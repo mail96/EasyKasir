4c882732c7363fe34f1b5eeb2d3a54c7f31116dc
djzamrud <asepsaifulzamrud@gmail.com>
2025-07-31 10:55:48 +0700

    ambil perubahan

--- easy_kasir/lib/main.dart
+++ easy_kasir/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:easy_kasir/login_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const LoginPage(),
+      home: const MyHomePage(title: 'Easy Kasir'),
     );
   }
 }
